6c8a27d9530d9cc7027709bd278b11d8aef8a51e
Vladimir <eurasian@list.ru>
2024-03-18 18:51:40 +0300

    Добавлено логгирование

--- src/ComplexCalculator.java
+++ src/ComplexCalculator.java
@@ -1,7 +1,12 @@
 import menu.UserMenu;
+import logger.Log;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class ComplexCalculator {
+    private static final Logger log = Log.log(ComplexCalculator.class.getName());
     public static void main(String[] args) {
+        log.log(Level.INFO, "Calculator starting...");
         UserMenu menu = new UserMenu();
         menu.userMenu();
     }
